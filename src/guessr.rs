7bb128dec44a10831a908fcc24f009a5d3dfe57a
Devon Burnham <novedevo@gmail.com>
2021-03-06 22:01:50 -0800

    add filtration and regex

--- src/guessr.rs
+++ src/guessr.rs
@@ -4,8 +4,8 @@ use std::error::Error;
 pub fn add_ordered(mut words: HashMap<String, f64>, csv_path: &str) -> Result<HashMap<String, f64>, Box<dyn Error>> {
     //honestly I have no idea what a dyn Error is
 
-    let mut rdr = csv::Reader::from_path(csv_path).unwrap();
-    type Record = (String, f64, u32, f64, f64); //structure of the
+    let mut rdr = csv::Reader::from_path(csv_path)?; //passes errors to caller
+    type Record = (String, f64, u32, f64, f64); //structure of the csv
 
     for result in rdr.deserialize() {
         let record: Record = result?;
@@ -21,9 +21,35 @@ pub fn add_unordered(
 ) -> Result<HashMap<String, f64>, Box<dyn Error>> {
     //honestly I have no idea what a dyn Error is
 
-    for line in std::fs::read_to_string(wordfile_path)?.lines() {
+    for line in std::fs::read_to_string(wordfile_path)?.lines() { //passes IO errors back to caller
         words.insert(String::from(line), 0.01);
     }
 
     Ok(words)
+}
+
+//TODO: refactor these filters once I learn how to use closures
+
+pub fn filter_length(words: HashMap<String, f64>, length: usize) -> HashMap<String, f64> {
+    let mut filtered_words = HashMap::with_capacity(words.len()/10);
+    for (word, prevalence) in words {
+        if word.len() == length {
+            filtered_words.insert(word, prevalence);
+        }
+    }
+    filtered_words
+}
+
+//TODO: see above
+
+pub fn filter_regex(words: HashMap<String, f64>, pattern: regex::Regex) -> HashMap<String, f64> {
+    let mut filtered_words = HashMap::with_capacity(words.len()/10);
+    
+    for (word, prevalence) in words {
+        if pattern.is_match(&word) {
+            filtered_words.insert(word, prevalence);
+        }
+    }
+    
+    filtered_words
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 mod guessr;
-
 use std::collections::HashMap;
 
 fn main() {
@@ -8,9 +7,15 @@ fn main() {
 
 fn test() {
     
-    let words: HashMap<String, f64> = HashMap::new();
+    let words: HashMap<String, f64> = HashMap::with_capacity(281700);
     
-    let words = guessr::add_ordered(words, "data/English_Word_Prevalences.csv").unwrap();
     let words = guessr::add_unordered(words, "data/words.txt").unwrap();
+    let words = guessr::add_ordered(words, "data/English_Word_Prevalences.csv").unwrap();
+    
+    let words = guessr::filter_length(words, 5);
+    
+    let words = guessr::filter_regex(words, regex::Regex::new("....E").unwrap());
+    
+    println!("{:?}", words)
     
 }
