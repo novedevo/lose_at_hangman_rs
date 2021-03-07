3b0362059cef35a67636ecc2df1e41f3925889ea
Devon Burnham <novedevo@gmail.com>
2021-03-06 21:21:16 -0800

    cleanup

--- src/guessr.rs
+++ src/guessr.rs
@@ -1,69 +1,29 @@
-extern crate csv;
 use std::collections::HashMap;
+use std::error::Error;
 
-// pub struct Guessr {
-//     scrabble_dict_string: String,
-//     alphabet: String,
-//     pub words: HashMap<String, f64>,
-// }
+pub fn add_ordered(mut words: HashMap<String, f64>, csv_path: &str) -> Result<HashMap<String, f64>, Box<dyn Error>> {
+    //honestly I have no idea what a dyn Error is
 
-// impl Guessr {
-//     pub fn guessr_builder() -> Guessr {
-//         let mut retval = Guessr {
-//             scrabble_dict_string: std::fs::read_to_string("data/words.txt").expect("File not found"),
-//             alphabet: String::from("ESIARNOTLCDUPMGHBYFKVWZXQJ-"),
-//             words: HashMap::new()
-//         };
-
-//         println!("hashmap created");
-//         retval.add_scrabble();
-//         println!("scrabblers added");
-//         retval.add_ordered();
-//         println!("orders added");
-
-//         retval
-//     }
-
-//     fn add_ordered(&mut self) {
-//         let mut rdr = csv::Reader::from_path("data/English_Word_Prevalences.csv").unwrap();
-//         type Record = (String, f64, u32, f64, f64);
-//         for result in rdr.deserialize() {
-//             let record: Record = match result {
-//                 Ok(r) => r,
-//                 _ => continue,
-//             };
-//             self.words.insert(record.0.to_uppercase(), record.1);
-//         }
-//     }
-
-//     fn add_scrabble(&mut self) {
-//         for line in self.scrabble_dict_string.lines() {
-//             self.words.insert(String::from(line), 0.01);
-//         }
-//     }
-// }
-
-pub fn add_ordered(
-    mut words: HashMap<String, f64>,
-    csv_path: &str,
-) -> Result<HashMap<String, f64>, Box<dyn std::error::Error>> {
     let mut rdr = csv::Reader::from_path(csv_path).unwrap();
-
-    type Record = (String, f64, u32, f64, f64);
+    type Record = (String, f64, u32, f64, f64); //structure of the
 
     for result in rdr.deserialize() {
         let record: Record = result?;
         words.insert(record.0.to_uppercase(), record.1);
     }
+
     Ok(words)
 }
 
 pub fn add_unordered(
     mut words: HashMap<String, f64>,
     wordfile_path: &str,
-) -> Result<HashMap<String, f64>, Box<dyn std::error::Error>> {
+) -> Result<HashMap<String, f64>, Box<dyn Error>> {
+    //honestly I have no idea what a dyn Error is
+
     for line in std::fs::read_to_string(wordfile_path)?.lines() {
         words.insert(String::from(line), 0.01);
     }
+
     Ok(words)
 }
