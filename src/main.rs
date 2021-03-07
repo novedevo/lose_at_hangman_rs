a961aa225c162dd61f6f8af58cd3ae14b621d9bc
Devon Burnham <novedevo@gmail.com>
2021-03-06 21:16:18 -0800

    remove pseudo-OO syntactic garbage

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 mod guessr;
 
+use std::collections::HashMap;
+
 fn main() {
     test();
 }
 
 fn test() {
-    let guesser = guessr::Guessr::guessr_builder();
-    println!("{}", guesser.words.len());
+    
+    let words: HashMap<String, f64> = HashMap::new();
+    
+    let words = guessr::add_ordered(words, "data/English_Word_Prevalences.csv").unwrap();
+    let words = guessr::add_unordered(words, "data/words.txt").unwrap();
+    
 }
